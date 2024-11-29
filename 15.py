07310a3c0b74e2d329a35b39d38113b93e2b607e
Jakiya23 <jakiyajahansoyna@gmail.com>
2024-11-29 19:45:59 +0600

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,23 @@
+# class car:
+#     def __init__(self,brand,model):
+#         self.brand= brand
+#         self.model= model
+
+#     def display_inf(self):
+#         print(f"car:{self.brand} {self.model}")
+
+# car1= car('Toyota','corolla')
+# car1. display_inf()
+
+
+class A():
+    def sound(sell):
+        print("Hello")
+
+class B(A):
+    def sound(sell):
+        super( ). sound
+        print("its ok")
+
+my= B()
+my. sound ()
